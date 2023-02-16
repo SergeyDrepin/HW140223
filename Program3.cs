0cd4f5866e9423f1aeaf67bb6d1dca3365e6aad0
Sergey Drepin <sdrepin@mail.ru>
2023-02-16 10:39:09 +0300

    Change name

---
+++ Program3.cs
@@ -0,0 +1,13 @@
+﻿int a = 34;
+int b = 5;
+int resalt = a % b;
+
+if(resalt == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.Write("Не кратно, остаток ");
+    Console.WriteLine(resalt);
+}
